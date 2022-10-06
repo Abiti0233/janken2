26a16e02cb33f689d7728f4a8d654e03c3ba5ea8
Abiti0233 <gogoyon3021@gmail.com>
2022-10-07 00:27:56 +0900

    メモアプリ

---
+++ memo.rb
@@ -0,0 +1,35 @@
+require "csv"
+
+puts "1(新規でメモを作成)" "2(既存のメモを編集する）"
+
+input_val1 = gets.to_i
+
+if input_val1 == 1
+  puts "拡張子を除いたファイル名を入力してください"
+file_name = gets.chomp
+
+  puts "メモの内容を入力してください。完了したらEnterを押してください"
+  
+input_memo = gets
+memo = input_memo.chomp
+
+  CSV.open("#{file_name}.csv","w") do |csv|
+    csv << ["#{memo}"]
+  end
+elsif input_val1 == 2
+  puts "編集するファイル名を入力してください"
+
+  file_name =gets.chomp
+
+  puts "メモの内容を入力してください。完了したらEnterを押してください。"
+
+ input_memo =gets
+ memo = input_memo.chomp
+
+ CSV.open("#{file_name}.csv","a") do |csv|
+  csv << ["#{memo}"]
+ end
+else
+  puts "1か2を入力してください。"
+end
+  
