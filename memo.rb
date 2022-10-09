0699ba1e266c3f53385ad1d90e80ccc2fd724d1a
Abiti0233 <gogoyon3021@gmail.com>
2022-10-09 14:16:11 +0900

    input_memo=getsをread,readlinesに変更

--- memo.rb
+++ memo.rb
@@ -8,9 +8,10 @@ if input_val1 == 1
   puts "拡張子を除いたファイル名を入力してください"
 file_name = gets.chomp
 
-  puts "メモの内容を入力してください。完了したらEnterを押してください"
+  puts "メモの内容を入力してください。"
+  puts "完了したらCtrl + Dを押してください"
   
-input_memo = gets
+input_memo = readlines
 memo = input_memo.chomp
 
   CSV.open("#{file_name}.csv","w") do |csv|
@@ -21,9 +22,10 @@ elsif input_val1 == 2
 
   file_name =gets.chomp
 
-  puts "メモの内容を入力してください。完了したらEnterを押してください。"
+  puts "メモの内容を入力してください。"
+  puts "完了したらCtrl + Dを押してください。"
 
- input_memo =gets
+ input_memo = readlines
  memo = input_memo.chomp
 
  CSV.open("#{file_name}.csv","a") do |csv|
